efdde35881b2f07b1e59ac35b447225d4aa99875
Bogdan Grigoruță <bogdangrigoruta@gmail.com>
2022-07-18 19:00:39 +0300

    feat: add compile function for fixed source code input

--- lua/godbolt/init.lua
+++ lua/godbolt/init.lua
@@ -1,55 +1,56 @@
-local curl = require "plenary.curl"
+local curl = require("plenary.curl")
 
 local M = {}
 
+M.bufnr = nil
 function M.languages()
-    local endpoint = "/api/languages"
-    local url =  "https://godbolt.org" .. endpoint
-    local resp = curl.get(url, {
-        accept = "application/json",
-    })
-    local contents = vim.fn.json_decode(resp.body)
-    vim.pretty_print(contents)
+  local endpoint = "/api/languages"
+  local url = "https://godbolt.org" .. endpoint
+  local resp = curl.get(url, {
+    accept = "application/json",
+  })
+  local contents = vim.fn.json_decode(resp.body)
+  vim.pretty_print(contents)
 end
 
 function M.compilers(lang)
-    local endpoint
-    if lang then
-        endpoint = "/api/compilers/" .. lang
-    else
-        endpoint = "/api/compilers"
-    end
+  local endpoint
+  if lang then
+    endpoint = "/api/compilers/" .. lang
+  else
+    endpoint = "/api/compilers"
+  end
 
-    local url =  "https://godbolt.org" .. endpoint
-    local resp = curl.get(url, {
-        accept = "application/json",
-    })
-    if resp.status ~= 200 then
-        error("bad request")
-    end
+  local url = "https://godbolt.org" .. endpoint
+  local resp = curl.get(url, {
+    accept = "application/json",
+  })
+  if resp.status ~= 200 then
+    error("bad request")
+  end
 
-    local compilers = vim.fn.json_decode(resp.body)
-    vim.pretty_print(compilers)
+  local compilers = vim.fn.json_decode(resp.body)
+  vim.pretty_print(compilers)
 end
 
 function M.libraries(lang)
-    local endpoint
-    if lang then
-        endpoint = "/api/libraries/" .. lang
-    else
-        endpoint = "/api/libraries"
-    end
+  local endpoint
+  if lang then
+    endpoint = "/api/libraries/" .. lang
+  else
+    endpoint = "/api/libraries"
+  end
 
-    local url =  "https://godbolt.org" .. endpoint
-    local resp = curl.get(url, {
-        accept = "application/json",
-    })
-    if resp.status ~= 200 then
-        error("bad request")
-    end
+  local url = "https://godbolt.org" .. endpoint
+  local resp = curl.get(url, {
+    accept = "application/json",
+  })
+  if resp.status ~= 200 then
+    error("bad request")
+  end
 
-    local libs = vim.fn.json_decode(resp.body)
-    vim.pretty_print(libs)
+  local libs = vim.fn.json_decode(resp.body)
+  vim.pretty_print(libs)
 end
 
 -- TODO
@@ -58,46 +59,56 @@ end
 
 -- TODO
 function M.compile()
-    local endpoint = "/api/compiler/g82/compile"
-    local url =  "https://godbolt.org" .. endpoint
-    local body = {
-        source = "int main () { return 1; }",
-        compiler = "g82",
-        options = {
-            userArguments = "-O3",
-            executeParameters = {
-                args = { "arg1", "arg2" },
-                stdin = "hello, world!"
-            },
-            compilerOptions = {
-                executorRequest = true
-            },
-            filters = {
-                execute = true
-            },
-            tools = { },
-            libraries = { {
-                id = "openssl",
-                version = "111c"
-            } }
-        },
-        lang = "c++",
-        allowStoreCodeDebug = true
-    }
+  local endpoint = "/api/compiler/g82/compile"
+  local url = "https://godbolt.org" .. endpoint
+  local body = {
+    source = "int square(int num) {\n return num * 3;\n}",
+    compiler = "g121",
+    lang = "c++",
+    allowStoreCodeDebug = true,
+    options = {
+      filters = {},
+      libraries = {},
+      tools = {},
+      compilerOptions = {},
+      userArguments = "",
+    },
+  }
 
-    local resp = curl.post(url, {
-        body = vim.fn.json_encode(body),
-        headers = {
-          content_type = "application/json",
-          accept = "application/json",
-          accept_encoding= ''
-        },
-    })
-    if resp.status ~= 200 then
-        error("bad request")
-    end
-    local libs = vim.fn.json_decode(resp.body)
-    vim.pretty_print(libs)
+  local resp = curl.post(url, {
+    body = vim.fn.json_encode(body),
+    headers = {
+      content_type = "application/json",
+      accept = "application/json",
+    },
+  })
+  if resp.status ~= 200 then
+    error("bad request")
+  end
+  local out = vim.fn.json_decode(resp.body)
+  local asm_lines = {}
+  for _, line in ipairs(out.asm) do
+    table.insert(asm_lines, line.text)
+  end
+
+  local name = "asm"
+  local buf = vim.fn.bufnr(name)
+  if buf == -1 then
+    buf = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_buf_set_name(buf, name)
+    vim.api.nvim_buf_set_option(buf, "ft", "asm")
+  end
+
+  if vim.fn.bufwinnr(buf) == -1 then
+    vim.cmd("vsplit")
+    local win = vim.api.nvim_get_current_win()
+    vim.api.nvim_win_set_buf(win, buf)
+
+    vim.api.nvim_buf_set_lines(buf, 0, 0, false, {})
+    vim.api.nvim_buf_set_lines(buf, 0, 0, false, asm_lines)
+  else
+    vim.cmd(vim.fn.bufwinnr(buf) .. "wincmd w")
+  end
 end
 
 M.compile()
