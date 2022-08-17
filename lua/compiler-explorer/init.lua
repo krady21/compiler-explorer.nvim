f2aece3945779f67ab043143d6a67ebff2e6445c
Bogdan Grigoruță <bogdangrigoruta@gmail.com>
2022-08-17 22:10:29 +0300

    feat: partial impl of the highlight autocommand source to asm

--- lua/compiler-explorer/init.lua
+++ lua/compiler-explorer/init.lua
@@ -23,8 +23,51 @@ function M.setup(user_config)
   config.setup(user_config or {})
 end
 
+local function create_autocmd(source_bufnr, asm_bufnr, dict)
+  local ns = vim.api.nvim_create_namespace("CompilerExplorer")
+  api.nvim_create_autocmd({ "CursorMoved" }, {
+    group = api.nvim_create_augroup("CompilerExplorer", { clear = true }),
+    buffer = source_bufnr,
+    callback = function()
+      -- clear previous highlights
+      api.nvim_buf_clear_namespace(asm_bufnr, ns, 0, -1)
+
+      local line_nr = fn.line(".")
+      local hl_ranges = dict[line_nr]
+      if hl_ranges == nil then
+        return
+      end
+
+      vim.highlight.range(asm_bufnr, ns, "Visual", { hl_ranges[1], 0 }, { hl_ranges[#hl_ranges], 0 }, "linewise", true)
+    end,
+  })
+end
+
+local function create_linehl_dict(asm)
+  local dict = {}
+  local prev_dict = {}
+  for asm_idx, line_obj in ipairs(asm) do
+    if line_obj.source ~= vim.NIL then
+      if line_obj.source.line ~= vim.NIL then
+        local source_idx = line_obj.source.line
+        if dict[source_idx] == nil then
+          dict[source_idx] = {}
+        end
+
+        table.insert(dict[source_idx], asm_idx)
+      end
+    end
+  end
+
+  -- { 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 22, 23 }
+  return dict
+end
+
 M.compile = async.void(function()
   local conf = config.get_config()
+
+  -- Get buffer number of the source code buffer
+  local source_bufnr = fn.bufnr("%")
 
   -- Get contents of current buffer
   local buf_contents = api.nvim_buf_get_lines(0, 0, -1, false)
@@ -75,20 +118,26 @@ M.compile = async.void(function()
   end
 
   local name = "asm"
-  local buf = fn.bufnr(name)
-  if buf == -1 then
-    buf = api.nvim_create_buf(false, true)
-    api.nvim_buf_set_name(buf, name)
-    api.nvim_buf_set_option(buf, "ft", "asm")
+  local asm_bufnr = fn.bufnr(name)
+  if asm_bufnr == -1 then
+    asm_bufnr = api.nvim_create_buf(false, true)
+    api.nvim_buf_set_name(asm_bufnr, name)
+    api.nvim_buf_set_option(asm_bufnr, "ft", "asm")
   end
 
-  if fn.bufwinnr(buf) == -1 then
+  if fn.bufwinnr(asm_bufnr) == -1 then
     cmd("vsplit")
     local win = api.nvim_get_current_win()
-    api.nvim_win_set_buf(win, buf)
+    api.nvim_win_set_buf(win, asm_bufnr)
   end
 
-  api.nvim_buf_set_lines(buf, 0, -1, false, asm_lines)
+  api.nvim_buf_set_lines(asm_bufnr, 0, -1, false, asm_lines)
+  vim.notify(string.format("Compilation done %s", compiler.name))
+
+  local source_to_asm_hl = create_linehl_dict(out.asm)
+  -- vim.pretty_print(source_to_asm_hl)
+
+  create_autocmd(source_bufnr, asm_bufnr, source_to_asm_hl)
 end)
 
 M.format = async.void(function()
@@ -106,7 +155,6 @@ M.format = async.void(function()
   })
 
   local style = formatter.styles[1] or "__DefaultStyle"
-  -- async.scheduler()
   if #formatter.styles > 0 then
     style = M.select(formatter.styles, {
       prompt = conf.prompt.formatter_style,
@@ -124,8 +172,8 @@ M.format = async.void(function()
   api.nvim_buf_set_lines(0, 0, -1, false, lines)
 
   -- TODO: Find how to make the text appear at the proper time.
-  async.scheduler()
   vim.notify(string.format("Text formatted using %s and style %s", formatter.name, style))
 end)
 
+-- vim.pretty_print(rest.languages_get())
 return M
