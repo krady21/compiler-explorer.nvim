krady21/compiler-explorer.nvim

commit 761db3a6f7398ebd472b220b5f4b2937bd0e1c82

fix: return early after cancelling vim.ui.select/input to avoid errors

lua/compiler-explorer/init.lua
@@ -74,6 +74,10 @@ M.compile = async.void(function(opts)
       })
     end
 
+    if not lang then
+      return
+    end
+
     -- Choose compiler
     local compilers = rest.compilers_get(lang.id)
     compiler = vim_select(compilers, {
@@ -83,8 +87,15 @@ M.compile = async.void(function(opts)
       end,
     })
 
+    if not compiler then
+      return
+    end
+
     -- Choose compiler options
     args.flags = vim_input({ prompt = "Select compiler options> ", default = conf.compiler_flags })
+    if args.flags then
+      return
+    end
     args.compiler = compiler.id
   end
 
@@ -208,6 +219,10 @@ M.add_library = async.void(function()
     })
   end
 
+  if not lang then
+    return
+  end
+
   local libs = rest.libraries_get(lang.id)
   if vim.tbl_isempty(libs) then
     alert.info("No libraries are available for %.", lang.name)
@@ -222,6 +237,10 @@ M.add_library = async.void(function()
     end,
   })
 
+  if not lib then
+    return
+  end
+
   -- Choose version
   local version = vim_select(lib.versions, {
     prompt = "Select library version> ",
@@ -229,6 +248,10 @@ M.add_library = async.void(function()
       return item.version
     end,
   })
+
+  if not version then
+    return
+  end
 
   -- Add lib to buffer variable, overwriting previous library version if already present
   vim.b.libs = vim.tbl_deep_extend("force", vim.b.libs or {}, { [lib.id] = version.version })
@@ -250,6 +273,9 @@ M.format = async.void(function()
       return item.name
     end,
   })
+  if not formatter then
+    return
+  end
 
   local style = formatter.styles[1] or "__DefaultStyle"
   if #formatter.styles > 0 then
@@ -259,6 +285,10 @@ M.format = async.void(function()
         return item
       end,
     })
+
+    if not style then
+      return
+    end
   end
 
   local body = rest.create_format_body(source, style)
@@ -331,6 +361,10 @@ M.load_example = async.void(function()
       return item
     end,
   })
+
+  if not lang_id then
+    return
+  end
 
   local example = vim_select(examples_by_lang[lang_id], {
     prompt = "Select example> ",

commit 033376a6f3bd5fb3138e14dca3bf1ee8895fa099

fix: return early from compile if *not* args.flags
bug introduced in https://github.com/krady21/compiler-explorer.nvim/commit/761db3a6f7398ebd472b220b5f4b2937bd0e1c82

lua/compiler-explorer/init.lua
@@ -93,7 +93,7 @@ M.compile = async.void(function(opts)
 
     -- Choose compiler options
     args.flags = vim_input({ prompt = "Select compiler options> ", default = conf.compiler_flags })
-    if args.flags then
+    if not args.flags then
       return
     end
     args.compiler = compiler.id