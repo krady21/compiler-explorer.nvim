953737417ad8752c85001ef56f64a13067d28998
Bogdan Grigoruță <bogdangrigoruta@gmail.com>
2023-02-06 21:16:39 +0200

    refactor: extract function for source <-> asm line matching callback

--- lua/compiler-explorer/autocmd.lua
+++ lua/compiler-explorer/autocmd.lua
@@ -41,28 +41,40 @@ M.create_autocmd = function(source_bufnr, asm_bufnr, resp, offset)
   local gid = api.nvim_create_augroup("CompilerExplorer" .. asm_bufnr, { clear = true })
   local ns = api.nvim_create_namespace("ce-autocmds")
 
+  local line_match_cb = function(other_buf, matching_lines)
+    if not api.nvim_buf_is_loaded(other_buf) then
+      api.nvim_clear_autocmds({ group = gid })
+      api.nvim_del_augroup_by_id(gid)
+      return
+    end
+
+    api.nvim_buf_clear_namespace(other_buf, ns, 0, -1)
+
+    local line_nr = fn.line(".")
+    local hl_list = matching_lines[line_nr]
+    if not hl_list then
+      return
+    end
+
+    if type(hl_list) ~= "table" then
+      hl_list = { hl_list }
+    end
+
+    for _, hl in ipairs(hl_list) do
+      -- highlight the matching line(s)
+      pcall(highlight_line, other_buf, hl - 1, ns, hl_group)
+    end
+
+    local winid = fn.bufwinid(other_buf)
+    -- move cursor to first matching line
+    pcall(api.nvim_win_set_cursor, winid, { hl_list[1], 0 })
+  end
+
   api.nvim_create_autocmd({ "CursorMoved" }, {
     group = gid,
     buffer = source_bufnr,
     callback = function()
-      if fn.bufloaded(asm_bufnr) == 0 then
-        api.nvim_clear_autocmds({ group = gid })
-        api.nvim_del_augroup_by_id(gid)
-        return
-      end
-      api.nvim_buf_clear_namespace(asm_bufnr, ns, 0, -1)
-
-      local line_nr = fn.line(".")
-      local hl_list = source_to_asm[line_nr]
-      if hl_list then
-        for _, hl in ipairs(hl_list) do
-          highlight_line(asm_bufnr, hl - 1, ns, hl_group)
-        end
-        local winid = fn.bufwinid(asm_bufnr)
-        if winid ~= -1 then
-          pcall(api.nvim_win_set_cursor, winid, { hl_list[1], 0 })
-        end
-      end
+      line_match_cb(asm_bufnr, source_to_asm)
     end,
   })
 
@@ -70,23 +82,7 @@ M.create_autocmd = function(source_bufnr, asm_bufnr, resp, offset)
     group = gid,
     buffer = asm_bufnr,
     callback = function()
-      if not fn.bufloaded(source_bufnr) == 0 then
-        api.nvim_clear_autocmds({ group = gid })
-        api.nvim_del_augroup_by_id(gid)
-        return
-      end
-      api.nvim_buf_clear_namespace(source_bufnr, ns, 0, -1)
-
-      local line_nr = fn.line(".")
-      local hl = asm_to_source[line_nr]
-      if hl then
-        highlight_line(source_bufnr, hl - 1, ns, hl_group)
-
-        local winid = fn.bufwinid(source_bufnr)
-        if winid ~= -1 then
-          pcall(api.nvim_win_set_cursor, winid, { hl, 0 })
-        end
-      end
+      line_match_cb(source_bufnr, asm_to_source)
     end,
   })
 
